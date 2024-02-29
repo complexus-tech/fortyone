8924ff6fdd541c36c01b82d6124367b891fb6e40
Joseph Mukorivo <josemukorivo@gmail.com>
2024-02-29 23:29:58 +0300

    Update UI components in my-issues/header.tsx and Popover.tsx

--- apps/projects/src/components/my-issues/header.tsx
+++ apps/projects/src/components/my-issues/header.tsx
@@ -1,5 +1,14 @@
 "use client";
-import { Box, BreadCrumbs, Button, Divider, Flex, Popover, Text } from "ui";
+import {
+  Box,
+  BreadCrumbs,
+  Button,
+  Divider,
+  Flex,
+  Popover,
+  Switch,
+  Text,
+} from "ui";
 import { ArrowDownIcon, IssueIcon, PreferencesIcon } from "icons";
 import { HeaderContainer } from "@/components/layout";
 import { SideDetailsSwitch } from "@/components/ui";
@@ -32,14 +41,57 @@ export const Header = ({
               size="sm"
               variant="outline"
             >
-              <span className="sr-onlyy">Display</span>
+              Display
             </Button>
           </Popover.Trigger>
-          <Popover.Content align="end">
+          <Popover.Content align="end" className="max-w-[24rem]">
+            <Flex
+              align="center"
+              className="my-2 px-4"
+              gap={2}
+              justify="between"
+            >
+              <Text color="muted">Group by</Text>
+              <Button
+                color="tertiary"
+                rightIcon={<ArrowDownIcon className="h-4 w-auto" />}
+                size="sm"
+              >
+                Status
+              </Button>
+            </Flex>
+            <Flex
+              align="center"
+              className="mb-3 px-4"
+              gap={2}
+              justify="between"
+            >
+              <Text color="muted">Order by</Text>
+              <Button
+                color="tertiary"
+                rightIcon={<ArrowDownIcon className="h-4 w-auto" />}
+                size="sm"
+              >
+                Priority
+              </Button>
+            </Flex>
+            <Divider className="my-2" />
             <Box className="max-w-[27rem] px-4 py-2">
+              <Text className="mb-4" fontWeight="medium">
+                Display options
+              </Text>
+              <Text className="mb-4" color="muted">
+                <label
+                  className="flex select-none items-center justify-between gap-2"
+                  htmlFor="more"
+                >
+                  Show empty columns <Switch id="more" />
+                </label>
+              </Text>
               <Text className="mb-2" color="muted">
                 Display columns
               </Text>
+
               <Flex gap={2} wrap>
                 <Button rounded="sm" size="xs">
                   Status
@@ -59,9 +111,38 @@ export const Header = ({
                 <Button color="tertiary" rounded="sm" size="xs">
                   Updated
                 </Button>
+                <Button color="tertiary" rounded="sm" size="xs">
+                  Sprint
+                </Button>
+                <Button color="tertiary" rounded="sm" size="xs">
+                  Epic
+                </Button>
+                <Button color="tertiary" rounded="sm" size="xs">
+                  Labels
+                </Button>
               </Flex>
             </Box>
-            <Divider className="my-2" />
+            <Divider className="mb-3 mt-2" />
+            <Text className="mb-2 px-4" color="muted">
+              <label
+                className="flex select-none items-center justify-between gap-2"
+                htmlFor="more"
+              >
+                Show empty groups <Switch id="more" />
+              </label>
+            </Text>
+            <Text className="mb-3 px-4" color="muted">
+              <label
+                className="flex select-none items-center justify-between gap-2"
+                htmlFor="more"
+              >
+                Show sub issues <Switch id="more" />
+              </label>
+            </Text>
+            <Divider className="mb-3" />
+            <Text className="mb-2 px-4" color="primary" fontWeight="medium">
+              Reset to default
+            </Text>
           </Popover.Content>
         </Popover>
 
