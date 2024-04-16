8e698b0491c941db3c16692cea0899c67e5d24a7
Joseph Mukorivo <josemukorivo@gmail.com>
2024-04-16 13:05:53 +0300

    Refactor home page components

--- apps/landing/src/components/pages/home/demo.tsx
+++ apps/landing/src/components/pages/home/demo.tsx
@@ -2,31 +2,42 @@
 import { Box } from "ui";
 import { Container, Blur } from "@/components/ui";
 import { useCursor } from "@/hooks";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export const ProductDemo = () => {
+  const [isActive, setIsActive] = useState(false);
   const cursor = useCursor();
   return (
     <Container className="relative">
       <Box
-        className="relative mx-auto aspect-[16/10] max-w-7xl overflow-hidden"
         onMouseEnter={() => {
           cursor.setText("See in action");
+          setIsActive(true);
         }}
         onMouseLeave={() => {
           cursor.removeText();
+          setIsActive(false);
         }}
       >
-        <video
-          autoPlay
-          className="pointer-events-none absolute -top-[4.6%] left-[10.95%] z-[2] mx-auto h-full w-[78%] object-contain opacity-90"
-          loop
-          muted
-          playsInline
-          src="/videos/demo.mp4"
-        />
-        <Box className="pointer-events-none absolute left-0 top-0 z-[3] h-full w-full overflow-hidden bg-[url(/images/device-mac.png)] bg-contain bg-center bg-no-repeat" />
+        <motion.div
+          animate={isActive ? { y: -6, x: 6 } : { y: 0, x: 0 }}
+          initial={{ y: 0, x: 0 }}
+          transition={{ type: "spring", stiffness: 100 }}
+          className="relative z-[2] mx-auto aspect-[16/10] max-w-7xl overflow-hidden"
+        >
+          <video
+            autoPlay
+            className="pointer-events-none absolute -top-[4.6%] left-[10.95%] z-[2] mx-auto h-full w-[78%] object-contain opacity-90"
+            loop
+            muted
+            playsInline
+            src="/videos/demo.mp4"
+          />
+          <Box className="pointer-events-none absolute left-0 top-0 z-[3] h-full w-full overflow-hidden bg-[url(/images/device-mac.png)] bg-contain bg-center bg-no-repeat" />
+        </motion.div>
       </Box>
-      <Blur className="absolute bottom-1/2 left-1/2 right-1/2 top-1/2 h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 bg-warning/10 md:h-[900px] md:w-[900px]" />
+      <Blur className="absolute bottom-1/2 left-1/2 right-1/2 top-1/2 z-0 h-[300px] w-[300px] -translate-x-1/2 -translate-y-1/2 bg-warning/10 md:h-[900px] md:w-[900px]" />
     </Container>
   );
 };
--- apps/landing/src/components/pages/home/hero.tsx
+++ apps/landing/src/components/pages/home/hero.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Button, Flex, Text, Box } from "ui";
 import { ArrowRightIcon } from "icons";
 import { Container } from "@/components/ui";
+import { motion } from "framer-motion";
 
 export const Hero = () => {
   return (
@@ -20,21 +22,42 @@ export const Hero = () => {
           >
             Announcing Private Beta 1.0
           </Button>
+          <motion.span
+            initial={{ y: 15, opacity: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{
+              duration: 1,
+              delay: 0.1,
+            }}
+            whileInView={{ y: 0, opacity: 1 }}
+          >
+            <Text
+              as="h1"
+              className="mt-6 max-w-5xl pb-2 text-6xl leading-none md:text-[5.5rem]"
+              color="gradient"
+            >
+              Nail every objective on time with complexus.
+            </Text>
+          </motion.span>
 
-          <Text
-            as="h1"
-            className="mt-6 max-w-5xl pb-2 text-6xl leading-none md:text-[5.5rem]"
-            color="gradient"
+          <motion.span
+            initial={{ y: 10, opacity: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{
+              duration: 1,
+              delay: 0.3,
+            }}
+            whileInView={{ y: 0, opacity: 1 }}
           >
-            Nail every objective on time with complexus.
-          </Text>
-          <Text
-            className="mt-6 max-w-[600px] text-lg opacity-90 md:mt-8 md:text-2xl"
-            fontWeight="normal"
-          >
-            Empower your team to crush every key objective with our seamless
-            project management platform.
-          </Text>
+            <Text
+              className="mt-6 max-w-[600px] text-lg opacity-90 md:mt-8 md:text-2xl"
+              fontWeight="normal"
+            >
+              Empower your team to crush every key objective with our seamless
+              project management platform.
+            </Text>
+          </motion.span>
+
           <Flex align="center" className="mt-10" gap={4}>
             <Button
               className="border border-primary"
