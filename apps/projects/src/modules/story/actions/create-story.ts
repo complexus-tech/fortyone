8c4411c12d27bb2b79aace8e4082d9c2eb997506
Joseph Mukorivo <josemukorivo@gmail.com>
2024-12-12 14:39:10 +0300

    fix: update actions

--- apps/projects/src/modules/story/actions/create-story.ts
+++ apps/projects/src/modules/story/actions/create-story.ts
@@ -4,13 +4,17 @@ import { post } from "@/lib/http";
 import { DetailedStory, NewStory } from "../types";
 import { revalidateTag } from "next/cache";
 import { storyTags } from "@/modules/stories/constants";
+import { ApiResponse } from "@/types";
 
 export const createStoryAction = async (newStory: NewStory) => {
-  const story = await post<NewStory, DetailedStory>("stories", newStory);
+  const story = await post<NewStory, ApiResponse<DetailedStory>>(
+    "stories",
+    newStory,
+  );
   revalidateTag(storyTags.mine());
   revalidateTag(storyTags.teams());
   if (newStory.parentId) {
     revalidateTag(storyTags.detail(newStory.parentId));
   }
-  return story;
+  return story.data!;
 };
