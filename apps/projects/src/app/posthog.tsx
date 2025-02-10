23f23993ffba412730a4cd1f1d364898e9b9b095
Joseph Mukorivo <josemukorivo@gmail.com>
2025-02-10 21:25:55 +0300

    chore: update PostHog dependencies and configuration

--- apps/projects/src/app/posthog.tsx
+++ apps/projects/src/app/posthog.tsx
@@ -5,7 +5,8 @@ import type { ReactNode } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     capture_pageview: false,
     capture_pageleave: true,
