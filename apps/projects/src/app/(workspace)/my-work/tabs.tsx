3f8cf66a79aa6f006990f0581075b76fc068baef
Joseph Mukorivo <josemukorivo@gmail.com>
2025-02-11 01:46:14 +0300

    feat: add loading skeleton for My Work page

---
+++ apps/projects/src/app/(workspace)/my-work/tabs.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Tabs } from "ui";
+
+export const MainTabs = () => {
+  return (
+    <Tabs defaultValue="assigned">
+      <Tabs.List>
+        <Tabs.Tab value="summary">Summary</Tabs.Tab>
+        <Tabs.Tab value="assigned">Assigned</Tabs.Tab>
+        <Tabs.Tab value="created">Created</Tabs.Tab>
+        <Tabs.Tab value="subscribed">Subscribed</Tabs.Tab>
+      </Tabs.List>
+    </Tabs>
+  );
+};
