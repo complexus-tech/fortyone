a7f42d1e4c90d9088d93cd7ad2f30a686fd9a048
Joseph Mukorivo <josemukorivo@gmail.com>
2025-02-10 23:12:11 +0300

    refactor: remove created_at and updated_at from key results insert query

--- internal/core/keyresults/keyresultsrepo/keyresults.go
+++ internal/core/keyresults/keyresultsrepo/keyresults.go
@@ -49,12 +49,11 @@ func (r *repo) Create(ctx context.Context, kr *CoreKeyResult) error {
 	const q = `
 		INSERT INTO key_results (
 			objective_id, name, measurement_type,
-			start_value, current_value, target_value, 
-			created_at, updated_at, created_by, last_updated_by
+			start_value, current_value, target_value, created_by, last_updated_by
 		) VALUES (
 			:objective_id, :name, :measurement_type,
 			:start_value, :current_value, :target_value, 
-			:created_at, :updated_at, :created_by, :last_updated_by
+			:created_by, :last_updated_by
 		)
 	`
 
