ee4521e38eb2c13a276756a8f26afe2cc6921b47
Joseph Mukorivo <josemukorivo@gmail.com>
2025-02-17 22:48:12 +0300

    fix: ensure workspace retrieval after revalidation

--- apps/projects/src/lib/http/fetch.ts
+++ apps/projects/src/lib/http/fetch.ts
@@ -2,7 +2,9 @@
 import type { Options } from "ky";
 import ky from "ky";
 import { headers } from "next/headers";
+import { revalidateTag } from "next/cache";
 import { auth } from "@/auth";
+import { workspaceTags } from "@/constants/keys";
 import { getWorkspaces } from "../queries/workspaces/get-workspaces";
 import { ApiError } from "./error";
 
@@ -13,8 +15,15 @@ const createClient = async () => {
   const subdomain = headersList.get("host")!.split(".")[0];
   const session = await auth();
 
-  const workspaces = await getWorkspaces(session!.token);
-  const workspace = workspaces.find(
+  let workspaces = await getWorkspaces(session!.token);
+  let workspace = workspaces.find(
+    (w) => w.slug.toLowerCase() === subdomain.toLowerCase(),
+  );
+  if (!workspace) {
+    revalidateTag(workspaceTags.lists());
+  }
+  workspaces = await getWorkspaces(session!.token);
+  workspace = workspaces.find(
     (w) => w.slug.toLowerCase() === subdomain.toLowerCase(),
   );
 
