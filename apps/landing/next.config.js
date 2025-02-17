2b96abc0112d4971c6cf72245a37da4d429d46aa
Joseph Mukorivo <josemukorivo@gmail.com>
2025-02-17 17:02:37 +0300

    feat: configure PostHog analytics with custom ingestion routes

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui", "icons"],
-
   images: {
     remotePatterns: [
       {
@@ -13,5 +12,21 @@ module.exports = {
         hostname: "images.unsplash.com",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
   },
 };
--- apps/landing/src/app/posthog.tsx
+++ apps/landing/src/app/posthog.tsx
@@ -5,12 +5,11 @@ import type { ReactNode } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     capture_pageview: false,
     capture_pageleave: true,
-
-    disable_session_recording: true,
   });
 }
 export const PostHogProvider = ({ children }: { children: ReactNode }) => {
