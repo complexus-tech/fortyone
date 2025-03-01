966b7aa0e8b9b86d2cd380f6ea1dcf56fc05336e
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-02 00:55:06 +0300

    refactor: remove server-side caching from member queries

--- apps/projects/src/lib/queries/members/get-members.ts
+++ apps/projects/src/lib/queries/members/get-members.ts
@@ -1,25 +1,14 @@
 "use server";
-import { memberTags } from "@/constants/keys";
-import { DURATION_FROM_SECONDS } from "@/constants/time";
+
 import { get } from "@/lib/http";
 import type { ApiResponse, Member } from "@/types";
 
 export const getMembers = async () => {
-  const members = await get<ApiResponse<Member[]>>("members", {
-    next: {
-      revalidate: DURATION_FROM_SECONDS.MINUTE * 10,
-      tags: [memberTags.lists()],
-    },
-  });
+  const members = await get<ApiResponse<Member[]>>("members");
   return members.data!;
 };
 
 export const getTeamMembers = async (teamId: string) => {
-  const members = await get<ApiResponse<Member[]>>(`members?teamId=${teamId}`, {
-    next: {
-      revalidate: DURATION_FROM_SECONDS.MINUTE * 10,
-      tags: [memberTags.team(teamId)],
-    },
-  });
+  const members = await get<ApiResponse<Member[]>>(`members?teamId=${teamId}`);
   return members.data!;
 };
