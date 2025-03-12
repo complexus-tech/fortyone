e9f694527111025b99f2aa42a126365d6009edf4
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-12 03:05:33 +0300

    refactor: enforce non-optional teamId in team objectives hook

--- apps/projects/src/modules/objectives/hooks/use-objectives.ts
+++ apps/projects/src/modules/objectives/hooks/use-objectives.ts
@@ -10,7 +10,7 @@ export const useObjectives = () => {
   });
 };
 
-export const useTeamObjectives = (teamId?: string) => {
+export const useTeamObjectives = (teamId: string) => {
   return useQuery({
     queryKey: objectiveKeys.team(teamId),
     queryFn: () => getTeamObjectives(teamId),
