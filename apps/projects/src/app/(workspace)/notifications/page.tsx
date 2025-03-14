2360dd25e5e30790a5ff318766ab1208fa6e9ff0
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-14 21:17:48 +0300

    refactor: optimize page components by implementing hydration boundaries and prefetching for stories, notifications, and summary data

--- apps/projects/src/app/(workspace)/notifications/page.tsx
+++ apps/projects/src/app/(workspace)/notifications/page.tsx
@@ -1,15 +1,30 @@
 import type { Metadata } from "next";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { BodyContainer } from "@/components/shared";
 import { ListNotifications } from "@/modules/notifications/list";
+import { getQueryClient } from "@/app/get-query-client";
+import { notificationKeys } from "@/constants/keys";
+import { getNotifications } from "@/modules/notifications/queries/get-notifications";
 
 export const metadata: Metadata = {
   title: "Notifications",
 };
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = getQueryClient();
+
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: notificationKeys.all,
+      queryFn: getNotifications,
+    }),
+  ]);
+
   return (
-    <BodyContainer className="h-screen">
-      <ListNotifications />
-    </BodyContainer>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <BodyContainer className="h-screen">
+        <ListNotifications />
+      </BodyContainer>
+    </HydrationBoundary>
   );
 }
