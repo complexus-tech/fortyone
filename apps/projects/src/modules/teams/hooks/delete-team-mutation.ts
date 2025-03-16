447de74addfbc0d8ea99af65db8fc0f939f60689
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-16 19:57:58 +0300

    refactor: enhance delete team mutation to redirect to my work page upon successful deletion using router

--- apps/projects/src/modules/teams/hooks/delete-team-mutation.ts
+++ apps/projects/src/modules/teams/hooks/delete-team-mutation.ts
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { useParams, useRouter } from "next/navigation";
 import { useAnalytics } from "@/hooks";
 import { teamKeys } from "@/constants/keys";
 import { deleteTeamAction } from "../actions/delete-team";
 
 export const useDeleteTeamMutation = () => {
+  const { teamId: teamIdParam } = useParams<{ teamId?: string }>();
   const queryClient = useQueryClient();
   const { analytics } = useAnalytics();
   const toastId = "delete-team";
+  const router = useRouter();
 
   const mutation = useMutation({
     mutationFn: (id: string) => deleteTeamAction(id),
@@ -36,17 +39,19 @@ export const useDeleteTeamMutation = () => {
       if (res.error?.message) {
         throw new Error(res.error.message);
       }
-
       // Track team deletion
       analytics.track("team_deleted", {
         teamId,
       });
-
       toast.success("Success", {
         description: "Team deleted successfully",
         id: toastId,
       });
       queryClient.invalidateQueries({ queryKey: teamKeys.lists() });
+      if (teamIdParam) {
+        // If the team is deleted from the team page, redirect to the my work page
+        router.push("/my-work");
+      }
     },
   });
 
