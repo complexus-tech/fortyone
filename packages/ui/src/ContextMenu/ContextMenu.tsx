b4f94f03e827980015e2e23b35198c6c45006fe4
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-20 02:40:46 +0300

    refactor: enhance ContextMenu, Menu, and Popover components by updating shadow styles for improved visual consistency

--- packages/ui/src/ContextMenu/ContextMenu.tsx
+++ packages/ui/src/ContextMenu/ContextMenu.tsx
@@ -22,7 +22,7 @@ const Trigger = ({ children, className, ...rest }: TriggerProps) => (
 );
 
 const contentClasses = cva(
-  "bg-white/80 dark:bg-dark-200/90 backdrop-blur z-50 border border-gray-50 dark:border-dark-50 w-max shadow-sm shadow-dark/10 dark:shadow-dark/20 mt-1 py-1.5",
+  "bg-white/80 dark:bg-dark-200/90 backdrop-blur z-50 border border-gray-50 dark:border-dark-50 w-max shadow shadow-gray-100 dark:shadow-dark/20 mt-1 py-1.5",
   {
     variants: {
       rounded: {
