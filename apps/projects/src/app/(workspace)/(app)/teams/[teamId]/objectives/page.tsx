7cca94a04975e6de636c6bb656d9271e86a3d907
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-20 01:44:23 +0300

    refactor: simplify Page component by removing async data fetching and HydrationBoundary; directly render TeamObjectivesList for improved performance

--- apps/projects/src/app/(workspace)/(app)/teams/[teamId]/objectives/page.tsx
+++ apps/projects/src/app/(workspace)/(app)/teams/[teamId]/objectives/page.tsx
@@ -1,28 +1,5 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { TeamObjectivesList } from "@/modules/objectives";
-import { getQueryClient } from "@/app/get-query-client";
-import { getTeamObjectives } from "@/modules/objectives/queries/get-team-objectives";
-import { objectiveKeys } from "@/modules/objectives/constants";
 
-export default async function Page(props: {
-  params: Promise<{
-    teamId: string;
-  }>;
-}) {
-  const params = await props.params;
-
-  const { teamId } = params;
-
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: objectiveKeys.team(teamId),
-    queryFn: () => getTeamObjectives(teamId),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <TeamObjectivesList />;
-    </HydrationBoundary>
-  );
+export default function Page() {
+  return <TeamObjectivesList />;
 }
