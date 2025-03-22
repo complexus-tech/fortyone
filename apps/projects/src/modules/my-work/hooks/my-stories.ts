fb79240185417eca60f2c7c63c0c306b1f1a7425
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-22 15:43:15 +0300

    refactor: replace useSuspenseQuery with useQuery in useMyStories hook for improved data fetching consistency

--- apps/projects/src/modules/my-work/hooks/my-stories.ts
+++ apps/projects/src/modules/my-work/hooks/my-stories.ts
@@ -1,9 +1,9 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { storyKeys } from "@/modules/stories/constants";
 import { getMyStories } from "../queries/get-stories";
 
 export const useMyStories = () => {
-  return useSuspenseQuery({
+  return useQuery({
     queryKey: storyKeys.mine(),
     queryFn: getMyStories,
   });
