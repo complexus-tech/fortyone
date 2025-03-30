413e61ffd0d03bdc78db5eb2f1b531267bd38b6f
Joseph Mukorivo <josemukorivo@gmail.com>
2025-03-30 11:25:29 +0300

    feat: add stale time configuration for story comments and story detail queries to enhance data freshness

--- apps/projects/src/modules/story/hooks/story.ts
+++ apps/projects/src/modules/story/hooks/story.ts
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { storyKeys } from "@/modules/stories/constants";
+import { DURATION_FROM_MILLISECONDS } from "@/constants/time";
 import { getStory } from "../queries/get-story";
 
 export const useStoryById = (id: string) => {
   return useQuery({
     queryKey: storyKeys.detail(id),
     queryFn: () => getStory(id),
+    staleTime: DURATION_FROM_MILLISECONDS.MINUTE * 3,
   });
 };
