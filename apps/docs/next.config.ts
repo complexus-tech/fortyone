d78ed4ddd0bf6761a4ca8171a2773af0985173be
Joseph Mukorivo <josemukorivo@gmail.com>
2025-04-05 15:53:01 +0300

    feat: add docs

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import nextra from "nextra";
+
+const withNextra = nextra({
+  search: {
+    codeblocks: false,
+  },
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withNextra(nextConfig);
