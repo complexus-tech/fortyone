95f2f48f0c716b93360bb103e023a69fb13156b9
Joseph Mukorivo <josemukorivo@gmail.com>
2025-05-16 05:29:00 +0300

    refactor: update layout configuration to enhance navigation and user experience with new links and improved structure

--- apps/docs/app/(docs)/layout.tsx
+++ apps/docs/app/(docs)/layout.tsx
@@ -1,11 +1,44 @@
-import { DocsLayout } from "fumadocs-ui/layouts/docs";
-import type { ReactNode } from "react";
+// import { DocsLayout } from "fumadocs-ui/layouts/docs";
+// import type { ReactNode } from "react";
+// import { baseOptions } from "@/app/layout.config";
+// import { source } from "@/lib/source";
+
+// export default function Layout({ children }: { children: ReactNode }) {
+//   return (
+//     <DocsLayout tree={source.pageTree} {...baseOptions}>
+//       {children}
+//     </DocsLayout>
+//   );
+// }
+
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
 import { baseOptions } from "@/app/layout.config";
 import { source } from "@/lib/source";
-
+import type { ReactNode } from "react";
+import { Library } from "lucide-react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout
+      {...baseOptions}
+      // the position of navbar
+      nav={{ ...baseOptions.nav, mode: "top" }}
+      // the position of Sidebar Tabs
+      tabMode="navbar"
+      themeSwitch={{
+        mode: "light-dark-system",
+      }}
+      links={[
+        {
+          text: "Sign up",
+          url: "https://www.complexus.app/signup",
+        },
+        {
+          text: "Login",
+          url: "https://www.complexus.app/login",
+        },
+      ]}
+      tree={source.pageTree}
+    >
       {children}
     </DocsLayout>
   );
