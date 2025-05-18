cfe934585384b6c9422ad0b0dcc271c0201f71de
Joseph Mukorivo <josemukorivo@gmail.com>
2025-05-18 14:50:18 +0300

    refactor: implement logout functionality with client storage clearance and loading state in logout page

--- apps/projects/src/app/logout/page.tsx
+++ apps/projects/src/app/logout/page.tsx
@@ -1,12 +1,62 @@
+"use client";
 import type { Metadata } from "next";
-import { redirect } from "next/navigation";
+import { Box, Flex, Text } from "ui";
+import { useEffect } from "react";
 import { logOut } from "@/components/shared/sidebar/actions";
+import { ComplexusLogo } from "@/components/ui";
+import { useAnalytics } from "@/hooks";
 
-export const metadata: Metadata = {
-  title: "Login",
+const clearClientStorage = () => {
+  document.cookie.split(";").forEach((c) => {
+    document.cookie = c
+      .replace(/^ +/, "")
+      .replace(/=.*/, `=;expires=${new Date().toUTCString()};path=/`);
+  });
+  if ("caches" in window) {
+    caches.keys().then((names) => {
+      names.forEach((name) => {
+        caches.delete(name);
+      });
+    });
+  }
 };
 
-export default async function Page() {
-  await logOut();
-  return redirect("https://www.complexus.app/login");
+export const metadata: Metadata = {
+  title: "Logout",
+};
+
+export default function Page() {
+  const { analytics } = useAnalytics();
+
+  useEffect(() => {
+    const performLogout = async () => {
+      try {
+        await logOut();
+        analytics.logout(true);
+      } catch (error) {
+        // console.error("Error during logout process:", error);
+      } finally {
+        clearClientStorage();
+        window.location.href = "https://www.complexus.app?signedOut=true";
+      }
+    };
+
+    performLogout();
+  }, [analytics]);
+  return (
+    <Flex
+      align="center"
+      className="relative h-dvh dark:bg-black"
+      justify="center"
+    >
+      <Flex align="center" direction="column" justify="center">
+        <Box className="aspect-square w-max animate-pulse rounded-full bg-primary p-4">
+          <ComplexusLogo className="h-8 text-white" />
+        </Box>
+        <Text className="mt-4" color="muted" fontWeight="medium">
+          Logging out...
+        </Text>
+      </Flex>
+    </Flex>
+  );
 }
