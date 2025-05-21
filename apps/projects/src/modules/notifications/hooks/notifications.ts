fff7c391453f2e68e8307a560856fd9017614672
Joseph Mukorivo <josemukorivo@gmail.com>
2025-05-21 17:47:41 +0300

    fix: remove staleTime from useNotifications hook to optimize query performance

--- apps/projects/src/modules/notifications/hooks/notifications.ts
+++ apps/projects/src/modules/notifications/hooks/notifications.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { notificationKeys } from "@/constants/keys";
-import { DURATION_FROM_MILLISECONDS } from "@/constants/time";
 import { getNotifications } from "../queries/get-notifications";
 
 export const useNotifications = () => {
@@ -9,6 +8,5 @@ export const useNotifications = () => {
   return useQuery({
     queryKey: notificationKeys.all,
     queryFn: () => getNotifications(session!),
-    staleTime: DURATION_FROM_MILLISECONDS.MINUTE * 10,
   });
 };
