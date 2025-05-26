bf7cf29e72e9d2e57ea7d80a83a7394ac21441ae
Joseph Mukorivo <josemukorivo@gmail.com>
2025-05-26 11:42:52 +0300

    fix: adjust span text color in renderTemplate for better contrast in notifications

--- apps/projects/src/modules/notifications/utils/render-template.ts
+++ apps/projects/src/modules/notifications/utils/render-template.ts
@@ -18,7 +18,7 @@ export const renderTemplate = (
 
       if (variable.value) {
         if (wrapInSpan) {
-          return `<span class="font-semibold antialiased text-black/90 dark:text-gray-200">${variable.value}</span>`;
+          return `<span class="font-semibold antialiased text-black/80 dark:text-gray-200/80">${variable.value}</span>`;
         }
         return variable.value;
       }
