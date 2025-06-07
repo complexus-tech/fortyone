39e7bc0825f87e3af432a288dab9b1cc0b825a65
Joseph Mukorivo <josemukorivo@gmail.com>
2025-06-07 13:29:26 +0300

    refactor: remove unused action exports from teams actions module for cleaner code structure

--- apps/projects/src/modules/teams/actions/index.ts
+++ apps/projects/src/modules/teams/actions/index.ts
@@ -32,6 +32,3 @@ export async function updateTeam(teamId: string, input: UpdateTeamInput) {
     return getApiError(error);
   }
 }
-
-export { updateSprintSettingsAction } from "./update-sprint-settings";
-export { updateStoryAutomationSettingsAction } from "./update-story-automation-settings";
