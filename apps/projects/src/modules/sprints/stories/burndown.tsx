816aeda1c2ed619b385ce3c42098875903bfd774
Joseph Mukorivo <josemukorivo@gmail.com>
2025-06-10 00:23:33 +0300

    refactor: streamline CustomTooltip styling in BurndownChart by removing unnecessary padding for enhanced readability

--- apps/projects/src/modules/sprints/stories/burndown.tsx
+++ apps/projects/src/modules/sprints/stories/burndown.tsx
@@ -32,7 +32,7 @@ const CustomTooltip = ({
   if (active && payload?.length) {
     const data = payload[0].payload;
     return (
-      <Box className="z-50 min-w-44 rounded-2xl border border-gray-100 bg-white/80 p-3 text-[0.95rem] font-medium text-gray backdrop-blur dark:border-dark-50 dark:bg-dark-200/60 dark:text-gray-200">
+      <Box className="z-50 min-w-44 rounded-2xl border border-gray-100 bg-white/80 p-3 font-medium text-gray backdrop-blur dark:border-dark-50 dark:bg-dark-200/60 dark:text-gray-200">
         <Text fontWeight="semibold">{label}</Text>
         <Box className="mb-0.1 mt-1 text-warning">
           Actual: {data.actual}{" "}
