06cd12b9c9fe43344ca8adc8fdcc80de02ff7f18
Joseph Mukorivo <josemukorivo@gmail.com>
2025-06-10 17:37:29 +0300

    refactor: remove QueryClientProvider from RootLayout to simplify structure

--- apps/projects/src/app/(workspace)/(app)/layout.tsx
+++ apps/projects/src/app/(workspace)/(app)/layout.tsx
@@ -1,12 +1,6 @@
 import type { ReactNode } from "react";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { ApplicationLayout } from "@/components/layouts";
-import { getQueryClient } from "@/app/get-query-client";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <QueryClientProvider client={getQueryClient()}>
-      <ApplicationLayout>{children}</ApplicationLayout>
-    </QueryClientProvider>
-  );
+  return <ApplicationLayout>{children}</ApplicationLayout>;
 }
