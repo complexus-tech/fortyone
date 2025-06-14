46645337061deaebbbf70a046a1ceafdff41097d
Joseph Mukorivo <josemukorivo@gmail.com>
2025-06-14 20:37:22 +0300

    refactor: update DragHandle component styling by adding strokeWidth for improved visual consistency

--- apps/projects/src/components/ui/story/drag-handle.tsx
+++ apps/projects/src/components/ui/story/drag-handle.tsx
@@ -4,7 +4,8 @@ import type { Icon } from "icons/src/types";
 export const DragHandle = (props: Icon) => {
   return (
     <DragIcon
-      className="absolute -left-[2.9rem] hidden h-5 w-auto cursor-move text-gray dark:text-gray-300 group-hover:md:inline-block"
+      className="absolute -left-[2.9rem] hidden cursor-move text-gray dark:text-gray-300 group-hover:md:inline-block"
+      strokeWidth={3.5}
       {...props}
     />
   );
