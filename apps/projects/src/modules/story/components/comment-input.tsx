46a50a27ce74fda49c2c9ad167fc3a84df9f3fb9
Joseph Mukorivo <josemukorivo@gmail.com>
2025-06-24 20:36:21 +0300

    refactor: update border color in CommentInput component for improved visual consistency

--- apps/projects/src/modules/story/components/comment-input.tsx
+++ apps/projects/src/modules/story/components/comment-input.tsx
@@ -220,7 +220,7 @@ export const CommentInput = ({
   return (
     <Flex
       className={cn(
-        "ml-1 min-h-[6rem] w-full rounded-2xl border border-gray-50 bg-gray-50/40 px-4 pb-4 text-[0.95rem] shadow shadow-gray-200 transition-shadow duration-200 ease-linear focus-within:shadow-lg dark:border-dark-200/80 dark:bg-dark-200/50 dark:shadow-dark-300/50",
+        "ml-1 min-h-[6rem] w-full rounded-2xl border border-gray-100 bg-gray-50/40 px-4 pb-4 text-[0.95rem] shadow shadow-gray-200 transition-shadow duration-200 ease-linear focus-within:shadow-lg dark:border-dark-200/80 dark:bg-dark-200/50 dark:shadow-dark-300/50",
         className,
       )}
       direction="column"
