7d6660e3f0ad6dce042c7c08b432b6a094bd2e18
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-18 15:26:47 +0300

    refactor: enhance chat history component with tooltip and menu for editing and deleting chats

--- apps/projects/src/components/ui/chat/history.tsx
+++ apps/projects/src/components/ui/chat/history.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Flex, Skeleton, Text } from "ui";
-import { ChatIcon, MoreHorizontalIcon } from "icons";
+import { Box, Button, Flex, Menu, Skeleton, Text, Tooltip } from "ui";
+import { ChatIcon, DeleteIcon, EditIcon, MoreHorizontalIcon } from "icons";
 import { useAiChats } from "@/modules/ai-chats/hooks/use-ai-chats";
 import { RowWrapper } from "../row-wrapper";
 
@@ -15,26 +15,48 @@ export const History = () => {
       </Box>
     );
   return (
-    <Box className="space-y-2 px-6">
+    <Box className="px-6">
       <Text className="mb-4 px-2">Today</Text>
       {chats.map((chat) => (
         <RowWrapper
-          className="px-2 first-of-type:border-t-[0.5px] md:px-1"
+          className="px-2 py-2 first-of-type:border-t-[0.5px] md:px-1"
           key={chat.id}
         >
-          <Flex align="center" gap={2}>
-            <ChatIcon className="h-4 shrink-0" />
-            <Text className="line-clamp-1">{chat.title}</Text>
-          </Flex>
-          <Button
-            asIcon
-            color="tertiary"
-            leftIcon={<MoreHorizontalIcon />}
-            size="sm"
-            variant="naked"
+          <Tooltip
+            title={
+              chat.createdAt ? new Date(chat.createdAt).toLocaleString() : null
+            }
           >
-            <span className="sr-only">Delete</span>
-          </Button>
+            <Flex align="center" className="flex-1 cursor-pointer" gap={2}>
+              <ChatIcon className="h-4 shrink-0" />
+              <Text className="line-clamp-1">{chat.title}</Text>
+            </Flex>
+          </Tooltip>
+          <Menu>
+            <Menu.Button>
+              <Button
+                asIcon
+                color="tertiary"
+                leftIcon={<MoreHorizontalIcon />}
+                size="sm"
+                variant="naked"
+              >
+                <span className="sr-only">Delete</span>
+              </Button>
+            </Menu.Button>
+            <Menu.Items align="end" className="w-28">
+              <Menu.Group>
+                <Menu.Item>
+                  <EditIcon />
+                  Rename
+                </Menu.Item>
+                <Menu.Item className="text-danger">
+                  <DeleteIcon className="h-4 text-danger dark:text-danger" />
+                  Delete
+                </Menu.Item>
+              </Menu.Group>
+            </Menu.Items>
+          </Menu>
         </RowWrapper>
       ))}
     </Box>
