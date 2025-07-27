8528ba81fbcf1456ade97bc9284fac34ea5727ad
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-27 22:39:47 +0300

    refactor: adjust text size in Hero component for improved visual consistency across devices

--- apps/landing/src/modules/home/hero.tsx
+++ apps/landing/src/modules/home/hero.tsx
@@ -46,7 +46,7 @@ export const Hero = () => {
           >
             <Text
               as="h1"
-              className="mt-6 pb-2 text-5xl font-semibold md:max-w-5xl md:text-[5rem] md:leading-[1.1]"
+              className="mt-6 pb-2 text-5xl font-semibold md:max-w-5xl md:text-[4.7rem] md:leading-[1.1]"
             >
               The{" "}
               <Text as="span" className="text-stroke-white">
