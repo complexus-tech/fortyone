f29cd4e4c2f5e31d83d496eb3f72d8ed09a8efe0
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-29 12:20:02 +0300

    refactor: simplify navigation component structure, enhance responsiveness, and update menu items for improved user experience

--- apps/sivio/src/components/shared/navigation.tsx
+++ apps/sivio/src/components/shared/navigation.tsx
@@ -1,87 +1,56 @@
 "use client";
 
-import { Container, NavigationMenu } from "ui";
-import { SprintsIcon, ObjectiveIcon, StoryIcon, OKRIcon } from "icons";
+import { Container, Flex, NavigationMenu } from "ui";
 import Link from "next/link";
-import type { ReactNode } from "react";
-
-const MenuItem = ({
-  name,
-
-  href,
-}: {
-  name: string;
-  description: string;
-  icon: ReactNode;
-  href: string;
-}) => (
-  <Link className="block text-lg" href={href}>
-    {name}
-  </Link>
-);
+import { Logo } from "./logo";
 
 export const Navigation = () => {
-  const features = [
+  const nav = [
     {
-      id: 1,
-      name: "Stories",
-      href: "/features/stories",
-      description: "Manage and Track Tasks",
-      icon: (
-        <StoryIcon className="relative h-6 w-auto shrink-0 md:top-1 md:h-4" />
-      ),
+      name: "About AfricaGiving",
+      items: [
+        { name: "About Us", href: "/about" },
+        { name: "Reporting", href: "/reporting" },
+        { name: "Contact Us", href: "/contact" },
+      ],
     },
     {
-      id: 2,
-      name: "Objectives",
-      href: "/features/objectives",
-      description: "Set and Achieve Goals",
-      icon: (
-        <ObjectiveIcon className="relative h-6 w-auto shrink-0 md:top-1 md:h-4" />
-      ),
-    },
-    {
-      id: 3,
-      name: "OKRs",
-      href: "/features/okrs",
-      description: "Align and Achieve",
-      icon: (
-        <OKRIcon className="relative h-6 w-auto shrink-0 md:top-1 md:h-4" />
-      ),
-    },
-    {
-      id: 4,
-      name: "Sprints",
-      href: "/features/sprints",
-      description: "Iterate and Deliver",
-      icon: (
-        <SprintsIcon className="relative h-6 w-auto shrink-0 md:top-1 md:h-4" />
-      ),
+      name: "For Donors",
+      items: [
+        { name: "For Individuals", href: "/for-individuals" },
+        { name: "For Corporates", href: "/for-corporates" },
+        { name: "Ways to Give", href: "/ways-to-give" },
+        { name: "Transparency", href: "/transparency" },
+      ],
     },
   ];
 
   return (
-    <Container as="nav">
-      <NavigationMenu>
-        <NavigationMenu.List>
-          <NavigationMenu.Item>
-            <NavigationMenu.Trigger className="text-2xl font-medium" hideArrow>
-              Product
-            </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="px-4">
-              {features.map(({ id, name, description, icon, href }) => (
-                <MenuItem
-                  description={description}
-                  href={href}
-                  icon={icon}
-                  key={id}
-                  name={name}
-                />
-              ))}
-            </NavigationMenu.Content>
-          </NavigationMenu.Item>
-        </NavigationMenu.List>
-      </NavigationMenu>
+    <Container as="nav" className="flex items-center justify-between py-2">
+      <Logo />
+      <Flex align="center" className="gap-10">
+        {nav.map((item, index) => (
+          <NavigationMenu key={index}>
+            <NavigationMenu.List>
+              <NavigationMenu.Item>
+                <NavigationMenu.Trigger
+                  className="text-2xl font-medium"
+                  hideArrow
+                >
+                  {item.name}
+                </NavigationMenu.Trigger>
+                <NavigationMenu.Content className="px-4">
+                  {item.items.map(({ name, href }) => (
+                    <Link className="block text-lg" href={href} key={name}>
+                      {name}
+                    </Link>
+                  ))}
+                </NavigationMenu.Content>
+              </NavigationMenu.Item>
+            </NavigationMenu.List>
+          </NavigationMenu>
+        ))}
+      </Flex>
     </Container>
   );
 };
