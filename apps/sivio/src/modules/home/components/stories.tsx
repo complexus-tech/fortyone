4b9052325468495cda7a8aa8aefd8957bfcac435
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-29 18:08:52 +0300

    refactor: add Stories component to HomePage for enhanced content presentation

---
+++ apps/sivio/src/modules/home/components/stories.tsx
@@ -0,0 +1,5 @@
+import { Box } from "ui";
+
+export const Stories = () => {
+  return <Box className="grid grid-cols-3">Stories</Box>;
+};
--- apps/sivio/src/modules/home/index.tsx
+++ apps/sivio/src/modules/home/index.tsx
@@ -1,12 +1,14 @@
 import { Box } from "ui";
 import { Hero } from "./components/hero";
 import { Support } from "./components/support";
+import { Stories } from "./components/stories";
 
 export const HomePage = () => {
   return (
     <Box>
       <Hero />
       <Support />
+      <Stories />
     </Box>
   );
 };
