f260ebe8d490aa3491f074aef764fa833927923f
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-29 17:48:00 +0300

    refactor: enhance Hero component with search functionality, improved layout, and additional content sections for better user engagement

--- apps/sivio/src/modules/home/components/hero.tsx
+++ apps/sivio/src/modules/home/components/hero.tsx
@@ -1,19 +1,39 @@
-import { BlurImage, Box, Flex, Text } from "ui";
+import { BlurImage, Box, Button, Flex, Input, Text } from "ui";
+import Image from "next/image";
+import { SearchIcon } from "icons";
 import { Container } from "@/components/ui";
+import educationSvg from "../../../../public/images/home/education.svg";
+import girlsRightsSvg from "../../../../public/images/home/girl.svg";
 
 export const Hero = () => {
   return (
     <Container className="mt-4">
       <Box className="relative">
         <BlurImage
-          className="aspect-video"
+          className="aspect-[16/10]"
           quality={100}
           src="/images/home/hero.webp"
         />
-        <Box className="absolute inset-0 grid grid-cols-2 bg-black/20">
-          Test
+        <Box className="absolute inset-0 grid grid-cols-2 bg-black/10">
+          <Flex className="px-10 py-16" direction="column" justify="end">
+            <Text as="h1" className="mb-4 text-5xl font-semibold text-white">
+              Exisiting Partner
+            </Text>
+            <Text className="mb-6 text-lg text-white">
+              Then search for the organisation to give towards
+            </Text>
+            <Input
+              className="rounded-xl bg-white"
+              leftIcon={<SearchIcon />}
+              placeholder="Search organisation by name, location or cause"
+              size="lg"
+            />
+            <Button className="ml-auto mt-4" color="secondary" size="lg">
+              Find organisation
+            </Button>
+          </Flex>
           <Flex
-            className="bg-secondary/80"
+            className="bg-secondary/50"
             direction="column"
             justify="between"
           >
@@ -27,9 +47,67 @@ export const Hero = () => {
               </Text>
             </Box>
             <Box className="grid grid-cols-3">
-              <Box>
-                <Text>Education</Text>
-              </Box>
+              <Flex className="h-44 bg-secondary" direction="column">
+                <Image
+                  alt="Education"
+                  className="h-24 w-auto invert"
+                  src={educationSvg}
+                />
+                <Text className="text-center text-2xl text-white">
+                  Education
+                </Text>
+              </Flex>
+              <Flex
+                className="h-44 bg-primary"
+                direction="column"
+                gap={4}
+                justify="center"
+              >
+                <Image
+                  alt="Education"
+                  className="h-24 w-auto invert"
+                  src={girlsRightsSvg}
+                />
+                <Text className="text-center text-2xl text-white">
+                  Girls rights
+                </Text>
+              </Flex>
+              <Flex
+                className="h-44 bg-secondary"
+                direction="column"
+                justify="center"
+              >
+                <Text className="text-center text-2xl text-white">
+                  Livelihoods & Economic Inclusion
+                </Text>
+              </Flex>
+              <Flex
+                className="h-44 bg-primary"
+                direction="column"
+                justify="center"
+              >
+                <Text className="text-center text-2xl text-white">
+                  Food Security
+                </Text>
+              </Flex>
+              <Flex
+                className="h-44 bg-secondary"
+                direction="column"
+                justify="center"
+              >
+                <Text className="text-center text-2xl text-white">
+                  Climate Change
+                </Text>
+              </Flex>
+              <Flex
+                className="h-44 bg-primary"
+                direction="column"
+                justify="center"
+              >
+                <Text className="text-center text-2xl text-white">
+                  Youth Empowerment
+                </Text>
+              </Flex>
             </Box>
           </Flex>
         </Box>
