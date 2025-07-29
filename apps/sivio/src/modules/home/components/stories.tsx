66d91576e38fe6b467c6a4a83f71a9d9fb2af1ae
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-29 18:13:43 +0300

    refactor: enhance Stories component with dynamic content and improved layout for better user engagement

--- apps/sivio/src/modules/home/components/stories.tsx
+++ apps/sivio/src/modules/home/components/stories.tsx
@@ -1,5 +1,36 @@
-import { Box } from "ui";
+import { Box, BlurImage, Text } from "ui";
 
 export const Stories = () => {
-  return <Box className="grid grid-cols-3">Stories</Box>;
+  const stories = [
+    {
+      title: "Zambia: Provide meals for children in ECD",
+      image: "/images/home/hero.webp",
+    },
+    {
+      title: "Kenya: Take orphaned girls to school",
+      image: "/images/home/hero.webp",
+    },
+    {
+      title:
+        "Zimbabwe: Support skills training for people living with disabilities ",
+      image: "/images/home/hero.webp",
+    },
+  ];
+
+  return (
+    <Box className="grid grid-cols-3">
+      <Box className="relative">
+        <BlurImage
+          className="aspect-square"
+          quality={100}
+          src="/images/home/hero.webp"
+        />
+        <Box className="absolute inset-0 flex flex-col justify-end bg-black/20 p-10">
+          <Text as="h2" className="w-full text-2xl font-semibold text-white">
+            Zambia: Provide meals for children in ECD
+          </Text>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
