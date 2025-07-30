016aabf3fc1a5c46ffdab2afafaf56b418b96a75
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-31 01:17:13 +0300

    refactor: optimize layout and styling of Hero and Subscribe components for enhanced user engagement and responsiveness

---
+++ apps/sivio/src/modules/organisations/resources/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "ui";
+import { Hero } from "./components/hero";
+import { Content } from "./components/content";
+
+export const ResourcesPage = () => {
+  return (
+    <Box>
+      <Hero />
+      <Content />
+    </Box>
+  );
+};
