e87864216761ec8055fb14b2d9fbbd67d5788516
Joseph Mukorivo <josemukorivo@gmail.com>
2025-07-30 16:20:57 +0300

    refactor: add Content component to TransparencyPage for enhanced layout and content presentation

--- apps/sivio/src/modules/organisations/transparency/components/content.tsx
+++ apps/sivio/src/modules/organisations/transparency/components/content.tsx
@@ -1,5 +1,17 @@
+import { BlurImage, Box } from "ui";
 import { Container } from "@/components/ui";
 
 export const Content = () => {
-  return <Container>C</Container>;
+  return (
+    <Container className="py-10">
+      <Box className="grid grid-cols-3 gap-10">
+        <BlurImage
+          alt="Transparency"
+          className="aspect-square"
+          src="/images/transparency/1.png"
+        />
+        <Box className="col-span-2">Test</Box>
+      </Box>
+    </Container>
+  );
 };
