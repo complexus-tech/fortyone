6ced53d98cf587a55212644722cf18794af86c32
Joseph Mukorivo <josemukorivo@gmail.com>
2025-08-05 22:02:24 +0300

    refactor: implement date range filter with presets and custom date selection, remove unused sprints filter component

--- apps/projects/src/modules/analytics/components/filters/index.tsx
+++ apps/projects/src/modules/analytics/components/filters/index.tsx
@@ -1,25 +1,15 @@
 "use client";
 import { Flex } from "ui";
-// import { DateRangeFilter } from "./date-range-filter";
+import { DateRangeFilter } from "./date-range-filter";
 import { TeamsFilter } from "./teams-filter";
-import { SprintsFilter } from "./sprints-filter";
 import { ObjectivesFilter } from "./objectives-filter";
 
 export const Filters = () => {
   return (
     <Flex align="end" gap={4}>
-      {/* <DateRangeFilter /> */}
+      <DateRangeFilter />
       <TeamsFilter />
-      <SprintsFilter />
       <ObjectivesFilter />
     </Flex>
   );
 };
-
-// Export individual components for potential standalone use
-// export { DateRangeFilter } from "./date-range-filter";
-export { TeamsFilter } from "./teams-filter";
-export { SprintsFilter } from "./sprints-filter";
-export { ObjectivesFilter } from "./objectives-filter";
-export { FilterButton } from "./filter-button";
-export * from "./types";
