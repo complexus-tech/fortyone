d59411b3e43bc9e048505c32cf68657e87943e65
Joseph Mukorivo <josemukorivo@gmail.com>
2025-08-27 21:01:13 +0300

    feat: implement custom Date type with JSON marshaling and unmarshaling
    - Introduced a new Date type that wraps time.Time, providing custom JSON marshaling and unmarshaling methods.
    - The UnmarshalJSON method parses date strings in the format "2006-01-02", while the MarshalJSON method formats the Date type back to a string.
    - This addition enhances date handling in the application, ensuring consistent date formatting in JSON interactions.

---
+++ pkg/date/date.go
@@ -0,0 +1,37 @@
+package date
+
+import (
+	"strings"
+	"time"
+)
+
+type Date time.Time
+
+func (d *Date) UnmarshalJSON(data []byte) error {
+
+	str := strings.Trim(string(data), `"`)
+
+	t, err := time.Parse("2006-01-02", str)
+	if err != nil {
+		return err
+	}
+
+	*d = Date(t)
+	return nil
+}
+
+func (d Date) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + time.Time(d).Format("2006-01-02") + `"`), nil
+}
+
+func (d Date) Time() time.Time {
+	return time.Time(d)
+}
+
+// func (d *Date) TimePtr() *time.Time {
+// 	if d == nil {
+// 		return nil
+// 	}
+// 	t := d.Time()
+// 	return &t
+// }
