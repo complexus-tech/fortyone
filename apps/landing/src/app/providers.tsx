e862bc6374cad82575ebc888039400446e36e167
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-03 23:37:00 +0300

    refactor: comment out unused authentication and query logic in RootLayout and Providers for improved clarity and maintainability

--- apps/landing/src/app/providers.tsx
+++ apps/landing/src/app/providers.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import type { Session } from "next-auth";
+// import type { Session } from "next-auth";
 import { CursorProvider } from "@/context";
 import { PostHogProvider } from "@/app/posthog";
 import PostHogPageView from "@/app/posthog-page-view";
@@ -15,14 +15,16 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export default function Providers({
   children,
-  session,
+  // session,
 }: {
   children: React.ReactNode;
-  session: Session | null;
+  // session: Session | null;
 }) {
   return (
     <ThemeProvider attribute="class" enableSystem>
-      <SessionProvider session={session}>
+      <SessionProvider
+      // session={session}
+      >
         <QueryClientProvider client={getQueryClient()}>
           <PostHogProvider>
             <CursorProvider>{children}</CursorProvider>
