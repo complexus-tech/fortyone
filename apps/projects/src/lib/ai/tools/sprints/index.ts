deb1c5dab6752435d4406134440354039c45dd22
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-05 17:15:30 +0300

    refactor: remove createSprint export from AI tools index to streamline code structure

--- apps/projects/src/lib/ai/tools/sprints/index.ts
+++ apps/projects/src/lib/ai/tools/sprints/index.ts
@@ -1,5 +1,4 @@
 export { listSprints } from "./list-sprints";
 export { listRunningSprints } from "./list-running-sprints";
 export { getSprintDetailsTool } from "./get-sprint-details";
-export { createSprint } from "./create-sprint";
 export { updateSprintSettings } from "./update-sprint-settings";
