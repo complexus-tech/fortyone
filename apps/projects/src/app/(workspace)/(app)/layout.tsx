621753abb5eff450dbe782966b632a81bc0f63b6
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-14 01:10:29 +0300

    refactor: simplify layout component by removing commented-out error handling UI for cleaner code

--- apps/projects/src/app/(workspace)/(app)/layout.tsx
+++ apps/projects/src/app/(workspace)/(app)/layout.tsx
@@ -6,28 +6,5 @@ export default async function RootLayout({
 }: {
   children: ReactNode;
 }) {
-  return (
-    <ApplicationLayout>
-      {/* <Box className="flex h-screen items-center justify-center">
-        <Box className="flex flex-col items-center">
-          <TeamIcon className="h-16 w-auto" />
-          <Text className="mb-6 mt-10" fontSize="3xl">
-            404: Team not found {data.error.message}
-          </Text>
-          <Text className="mb-6 max-w-md text-center" color="muted">
-            This team might not exist or you do not belong to this team.
-          </Text>
-          <Button
-            className="gap-1 pl-2"
-            color="tertiary"
-            href="/my-work"
-            leftIcon={<ArrowLeftIcon className="h-[1.05rem] w-auto" />}
-          >
-            Go to my work
-          </Button>
-        </Box>
-      </Box> */}
-      {children}]
-    </ApplicationLayout>
-  );
+  return <ApplicationLayout>{children}</ApplicationLayout>;
 }
