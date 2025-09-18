cda1ebffae51bfb0c8efeab7f4290f7f615aeda2
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-18 11:09:33 +0300

    refactor: update Avatar component color to primary in AssigneesMenu for enhanced visual consistency

--- apps/projects/src/components/ui/story/assignees-menu.tsx
+++ apps/projects/src/components/ui/story/assignees-menu.tsx
@@ -169,6 +169,7 @@ const Items = ({
               >
                 <Flex align="center" gap={2}>
                   <Avatar
+                    color="primary"
                     name={fullName || username}
                     size="sm"
                     src={avatarUrl}
