6ea65727be086f6a265f59fb8592bc55c8a8740f
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-20 14:26:09 +0300

    refactor: remove unnecessary state management in Providers component for improved performance and simplicity

--- apps/projects/src/app/providers.tsx
+++ apps/projects/src/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { useState, useEffect } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
@@ -14,15 +13,6 @@ import { PostHogProvider } from "./posthog";
 TimeAgo.addDefaultLocale(en);
 
 export const Providers = ({ children }: { children: ReactNode }) => {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>; // Render children without ThemeProvider during SSR
-  }
   return (
     <QueryClientProvider client={getQueryClient()}>
       <PostHogProvider>
