363f1e659002940dc937bb7e81f41f1a2ae48a64
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-25 15:40:32 +0300

    refactor: enhance RootLayout by adding a screen for tabs and remove deprecated index.tsx file

--- apps/mobile/app/(tabs)/about.tsx
+++ apps/mobile/app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome to FortyOne</Text>
+      <Text style={styles.text}>About</Text>
     </View>
   );
 }
---
+++ apps/mobile/app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Demo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
