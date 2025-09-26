016bcf9c7c1130ab322ba05c90ff88b3fa4e7401
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-26 16:15:07 +0300

    chore: update app.json and eas.json for mobile app configuration; enhance Index component with Airplane Mode toggle and context menu for improved user interaction

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,50 +1,95 @@
-import { Text, View, StyleSheet, Pressable, Button } from "react-native";
-import { ColorPicker, Host } from "@expo/ui/swift-ui";
+import {
+  Button,
+  ContextMenu,
+  Form,
+  Host,
+  HStack,
+  Image,
+  Picker,
+  Section,
+  Spacer,
+  Switch,
+  Text,
+} from "@expo/ui/swift-ui";
+import { background, clipShape, frame } from "@expo/ui/swift-ui/modifiers";
+import { Link } from "expo-router";
 import { useState } from "react";
 
 export default function Index() {
-  const [color, setColor] = useState("#000000");
+  const [isAirplaneMode, setIsAirplaneMode] = useState(true);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home</Text>
-      <Pressable
-        onPress={() => {
-          alert("Hello");
-        }}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Press me</Text>
-      </Pressable>
+    <Host style={{ flex: 1 }}>
+      <Form>
+        <Section>
+          <HStack spacing={8}>
+            <Image
+              systemName="airplane"
+              color="white"
+              size={18}
+              modifiers={[
+                frame({ width: 28, height: 28 }),
+                background("#ffa500"),
+                clipShape("roundedRectangle"),
+              ]}
+            />
+            <Text>Airplane Mode</Text>
+            <Text>Air</Text>
+            <Spacer />
+            <Switch value={isAirplaneMode} onValueChange={setIsAirplaneMode} />
+          </HStack>
 
-      <Button title="Press me" />
-      {/* 
-      <Host style={{ width: 400, height: 200 }}>
-        <ColorPicker
-          label="Select a color"
-          selection={color}
-          onValueChanged={setColor}
-        />
-      </Host> */}
-    </View>
+          <Link href="/inbox" asChild>
+            <Button>
+              <HStack spacing={8}>
+                <Image
+                  systemName="wifi"
+                  color="white"
+                  size={18}
+                  modifiers={[
+                    frame({ width: 28, height: 28 }),
+                    background("#007aff"),
+                    clipShape("roundedRectangle"),
+                  ]}
+                />
+                <Text color="primary">Wi-Fi</Text>
+                <Spacer />
+                <Image systemName="chevron.right" size={14} color="secondary" />
+              </HStack>
+            </Button>
+          </Link>
+
+          <ContextMenu>
+            <ContextMenu.Items>
+              <Button
+                systemImage="person.crop.circle.badge.xmark"
+                onPress={() => console.log("Pressed1")}
+              >
+                Hello
+              </Button>
+              <Button
+                variant="bordered"
+                systemImage="heart"
+                onPress={() => console.log("Pressed2")}
+              >
+                Love it
+              </Button>
+              <Picker
+                label="Doggos"
+                options={["very", "veery", "veeery", "much"]}
+                variant="menu"
+                selectedIndex={selectedIndex}
+                onOptionSelected={({ nativeEvent: { index } }) =>
+                  setSelectedIndex(index)
+                }
+              />
+            </ContextMenu.Items>
+            <ContextMenu.Trigger>
+              <Button variant="glass">Show Menu</Button>
+            </ContextMenu.Trigger>
+          </ContextMenu>
+        </Section>
+      </Form>
+    </Host>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-  button: {
-    backgroundColor: "#ffffff",
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: "#08090a",
-  },
-});
