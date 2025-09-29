7e29c1283abb36894f3a42f0b9dfca48b82f8e44
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-30 01:25:40 +0300

    refactor: integrate SafeAreaProvider and SafeAreaView in layout and Home components for improved safe area handling and responsiveness

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../styles/global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
--- apps/mobile/modules/home/index.tsx
+++ apps/mobile/modules/home/index.tsx
@@ -3,17 +3,20 @@ import { ScrollView } from "react-native";
 import { Header } from "./components/header";
 import { Container, Section } from "@/components/ui";
 import { Team } from "./components/team";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Home = () => {
   return (
     <Container>
-      <Header />
-      <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
-        <Section title="Your Teams">
-          <Team name="Engineering" color="#FF9500" />
-          <Team name="Complexus" color="#34C759" />
-        </Section>
-      </ScrollView>
+      <SafeAreaView>
+        <Header />
+        <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
+          <Section title="Your Teams">
+            <Team name="Engineering" color="#FF9500" />
+            <Team name="Complexus" color="#34C759" />
+          </Section>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 };
