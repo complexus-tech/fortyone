9f99afd573c8bfc6ec992981fcf3ffc35485b2e9
Joseph Mukorivo <josemukorivo@gmail.com>
2025-09-30 15:38:06 +0300

    refactor: enhance SearchScreen by integrating Maya component and optimizing search functionality for improved user experience

---
+++ apps/mobile/constants/colors.ts
@@ -0,0 +1,33 @@
+/**
+ * Matches the Tailwind configuration
+ */
+
+export const colors = {
+  primary: "#f43f5e",
+  secondary: "#002f61",
+  black: "#08090a",
+  white: "#ffffff",
+  transparent: "transparent",
+  current: "currentColor",
+  success: "#22c55e",
+  warning: "#eab308",
+  danger: "#f43f5e",
+  info: "#06b6d4",
+  sidebar: "#FCF5E5",
+  light: "#fffff0",
+  gray: {
+    DEFAULT: "#6B665C",
+    50: "#F8F6F2",
+    100: "#ECE9E4",
+    200: "#DAD6D0",
+    250: "#5E5A52",
+    300: "#A19B94",
+  },
+  dark: {
+    DEFAULT: "#0c0d0e",
+    50: "#25282b",
+    100: "#1d1f22",
+    200: "#16181a",
+    300: "#101214",
+  },
+} as const;
