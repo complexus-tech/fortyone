23c49efa37c34b6ada9612e455671e1b25be32cd
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-01 02:05:13 +0300

    refactor: update Header component styling for improved visual consistency and responsiveness

--- apps/mobile/modules/my-work/components/header.tsx
+++ apps/mobile/modules/my-work/components/header.tsx
@@ -12,9 +12,11 @@ export const Header = () => {
       </Text>
       <Pressable
         className="p-2 rounded-md"
-        style={({ pressed }) => [pressed && { backgroundColor: "#F2F2F7" }]}
+        style={({ pressed }) => [
+          pressed && { backgroundColor: colors.gray[50] },
+        ]}
       >
-        <SymbolView name="ellipsis" tintColor={colors.dark[50]} />
+        <SymbolView name="ellipsis" tintColor={colors.gray.DEFAULT} />
       </Pressable>
     </Row>
   );
