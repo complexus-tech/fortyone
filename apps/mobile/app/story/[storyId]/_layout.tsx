fdc32f1d88dbba21c8b8e315357886bd0e9f4549
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-01 17:31:30 +0300

    refactor: update icons in StoryLayout for improved clarity and consistency across tabs

--- apps/mobile/app/story/[storyId]/_layout.tsx
+++ apps/mobile/app/story/[storyId]/_layout.tsx
@@ -5,19 +5,22 @@ export default function StoryLayout() {
   return (
     <NativeTabs tintColor={colors.primary} minimizeBehavior="onScrollDown">
       <NativeTabs.Trigger name="index">
-        <Icon sf="doc.text.fill" />
+        <Icon sf="circle.grid.2x2.fill" />
         <Label>Overview</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="sub-stories">
-        <Icon sf="list.bullet" />
+        <Icon sf="checklist" />
         <Label>Sub Stories</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="links">
-        <Icon sf="link" />
+        <Icon sf="globe" />
         <Label>Links</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="attachments">
-        <Icon sf="paperclip" />
+        <Icon
+          sf="books.vertical.fill"
+          // sf="book.pages.fill"
+        />
         <Label>Attachments</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
