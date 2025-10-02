b89d2afa1dd3814fd4ef9d44c6d52ff63263083f
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-02 21:14:48 +0300

    refactor: integrate loadAuthData in RootLayout to enhance authentication state management

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,16 +1,20 @@
 import { Stack } from "expo-router";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../styles/global.css";
 import "react-native-svg";
+import { useAuthStore } from "@/store";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const loadAuthData = useAuthStore((state) => state.loadAuthData);
+
+  useEffect(() => {
+    loadAuthData();
+  }, [loadAuthData]);
   return (
-    <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="team/[teamId]" />
-        <Stack.Screen name="story/[storyId]" />
-      </Stack>
-    </SafeAreaProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="team/[teamId]" />
+      <Stack.Screen name="story/[storyId]" />
+    </Stack>
   );
 }
