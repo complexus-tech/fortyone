210c74d0176f4aff28cd7a6210210db839e27445
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-03 16:55:24 +0300

    refactor: add date-fns for date formatting and enhance Sprint Card status display with dynamic status colors

--- apps/mobile/modules/sprints/components/card.tsx
+++ apps/mobile/modules/sprints/components/card.tsx
@@ -3,8 +3,29 @@ import { Sprint } from "../types";
 import { Badge, Col, Row, Text } from "@/components/ui";
 import { SymbolView } from "expo-symbols";
 import { colors } from "@/constants/colors";
+import { format } from "date-fns";
+
+type SprintStatus = "completed" | "in progress" | "upcoming";
+
+const statusColors = {
+  completed: "tertiary",
+  "in progress": "success",
+  upcoming: "tertiary",
+} as const;
 
 export const Card = ({ sprint }: { sprint: Sprint }) => {
+  const startDateObj = new Date(sprint.startDate);
+  const endDateObj = new Date(sprint.endDate);
+  const now = new Date();
+
+  // Calculate sprint status
+  let sprintStatus: SprintStatus = "completed";
+  if (startDateObj <= now && endDateObj >= now) {
+    sprintStatus = "in progress";
+  } else if (startDateObj > now) {
+    sprintStatus = "upcoming";
+  }
+
   return (
     <Row
       align="center"
@@ -27,13 +48,15 @@ export const Card = ({ sprint }: { sprint: Sprint }) => {
               tintColor={colors.gray.DEFAULT}
             />
             <Text fontSize="sm" numberOfLines={1}>
-              {sprint.name}
+              {format(startDateObj, "MMM d")} → {format(endDateObj, "MMM d")}
             </Text>
           </Row>
         </Col>
       </Row>
-      <Badge color="tertiary" rounded="md">
-        <Text fontSize="sm">Upcoming</Text>
+      <Badge color={statusColors[sprintStatus]} rounded="md">
+        <Text fontSize="sm" className="capitalize">
+          {sprintStatus}
+        </Text>
       </Badge>
     </Row>
   );
