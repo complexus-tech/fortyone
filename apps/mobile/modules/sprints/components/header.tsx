57508653b2e0c73be07ffa16e1ac632934fd3d8f
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-03 18:28:13 +0300

    refactor: update Header component to display team name dynamically based on global search parameters

--- apps/mobile/modules/sprints/components/header.tsx
+++ apps/mobile/modules/sprints/components/header.tsx
@@ -3,13 +3,18 @@ import { Pressable } from "react-native";
 import { Text, Row, Back } from "@/components/ui";
 import { SymbolView } from "expo-symbols";
 import { colors } from "@/constants";
+import { useGlobalSearchParams } from "expo-router";
+import { useTeams } from "@/modules/teams/hooks/use-teams";
 
 export const Header = () => {
+  const { teamId } = useGlobalSearchParams<{ teamId: string }>();
+  const { data: teams = [] } = useTeams();
+  const { name } = teams.find((team) => team.id === teamId)!;
   return (
     <Row className="mb-3" asContainer justify="between" align="center">
       <Back />
       <Text fontSize="2xl" fontWeight="semibold">
-        Product /{" "}
+        {name} /{" "}
         <Text
           fontSize="2xl"
           color="muted"
