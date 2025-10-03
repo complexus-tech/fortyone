1704b1f81518dc9da7caf5bc70adfc4c265ad2d9
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-03 16:36:42 +0300

    refactor: enhance Sprint Card layout with icons and improved text presentation for better user experience

--- apps/mobile/modules/sprints/components/card.tsx
+++ apps/mobile/modules/sprints/components/card.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import { Sprint } from "../types";
-import { Badge, Row, Text } from "@/components/ui";
+import { Badge, Col, Row, Text } from "@/components/ui";
+import { SymbolView } from "expo-symbols";
+import { colors } from "@/constants/colors";
 
 export const Card = ({ sprint }: { sprint: Sprint }) => {
   return (
     <Row
       align="center"
       justify="between"
-      className="p-4 border-t border-gray-100"
+      className="px-4 py-3 border-t-[0.5px] border-gray-100"
     >
-      <Row>
-        <Text>{sprint.name}</Text>
+      <Row align="center" gap={3}>
+        <SymbolView
+          name="play.circle"
+          size={20}
+          weight="bold"
+          tintColor={colors.gray.DEFAULT}
+        />
+        <Col>
+          <Text numberOfLines={1}>{sprint.name}</Text>
+          <Row align="center" gap={1}>
+            <SymbolView
+              name="calendar"
+              size={20}
+              tintColor={colors.gray.DEFAULT}
+            />
+            <Text fontSize="sm" numberOfLines={1}>
+              {sprint.name}
+            </Text>
+          </Row>
+        </Col>
       </Row>
       <Badge color="tertiary" rounded="md">
         <Text fontSize="sm">Upcoming</Text>
