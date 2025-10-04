282e88768d184d3dea1cec662d9ab5951050bed4
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-04 15:27:06 +0300

    refactor: replace ScrollView and Card components with List in Sprints for improved readability and maintainability

---
+++ apps/mobile/modules/sprints/components/list.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { SafeContainer } from "@/components/ui";
+import { Header } from "@/modules/sprints/components/header";
+import { Card } from "@/modules/sprints/components/card";
+import { ScrollView } from "react-native";
+import { Sprint } from "@/modules/sprints/types";
+import { EmptyState } from "./empty-state";
+
+export const List = ({ sprints }: { sprints: Sprint[] }) => {
+  return (
+    <SafeContainer isFull>
+      <Header />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {sprints.length === 0 && <EmptyState />}
+        {sprints.map((sprint) => (
+          <Card key={sprint.id} sprint={sprint} />
+        ))}
+      </ScrollView>
+    </SafeContainer>
+  );
+};
--- apps/mobile/modules/sprints/index.tsx
+++ apps/mobile/modules/sprints/index.tsx
@@ -1,33 +1,18 @@
 import React from "react";
 import { SafeContainer } from "@/components/ui";
 import { Header } from "./components/header";
-import { Card } from "./components/card";
-import { EmptyState } from "./components/empty-state";
 import { useGlobalSearchParams } from "expo-router";
 import { useTeamSprints } from "./hooks";
-import { ScrollView } from "react-native";
+import { List } from "./components/list";
 
 export const Sprints = () => {
   const { teamId } = useGlobalSearchParams<{ teamId: string }>();
   const { data: sprints = [] } = useTeamSprints(teamId);
 
-  if (sprints.length === 0) {
-    return (
-      <SafeContainer isFull>
-        <Header />
-        <EmptyState />
-      </SafeContainer>
-    );
-  }
-
   return (
     <SafeContainer isFull>
       <Header />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {sprints.map((sprint) => (
-          <Card key={sprint.id} sprint={sprint} />
-        ))}
-      </ScrollView>
+      <List sprints={sprints} />
     </SafeContainer>
   );
 };
