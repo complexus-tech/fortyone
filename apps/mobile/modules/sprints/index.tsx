5233740e13294d1ca42c244570759ed7eee420dc
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-04 16:36:41 +0300

    refactor: simplify Sprints component by removing SafeContainer and Header for improved readability

--- apps/mobile/modules/sprints/index.tsx
+++ apps/mobile/modules/sprints/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { SafeContainer } from "@/components/ui";
-import { Header } from "./components/header";
 import { useGlobalSearchParams } from "expo-router";
 import { useTeamSprints } from "./hooks";
 import { List } from "./components/list";
@@ -9,10 +7,5 @@ export const Sprints = () => {
   const { teamId } = useGlobalSearchParams<{ teamId: string }>();
   const { data: sprints = [] } = useTeamSprints(teamId);
 
-  return (
-    <SafeContainer isFull>
-      <Header />
-      <List sprints={sprints} />
-    </SafeContainer>
-  );
+  return <List sprints={sprints} />;
 };
