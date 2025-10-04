9d341c930204bc8c5805beb1f10ea2225ea07742
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-04 19:40:39 +0300

    feat: implement dark mode support in Card component by utilizing color scheme handling for improved styling and responsiveness

--- apps/mobile/modules/objectives/components/card.tsx
+++ apps/mobile/modules/objectives/components/card.tsx
@@ -5,6 +5,7 @@ import { SymbolView } from "expo-symbols";
 import { colors } from "@/constants/colors";
 import { format } from "date-fns";
 import { Pressable } from "react-native";
+import { useColorScheme } from "nativewind";
 
 type ObjectiveHealth = "On Track" | "At Risk" | "Off Track" | null;
 
@@ -20,6 +21,7 @@ const getHealthColor = (health: ObjectiveHealth) => {
 };
 
 export const Card = ({ objective }: { objective: Objective }) => {
+  const { colorScheme } = useColorScheme();
   const startDateObj = new Date(objective.startDate);
   const endDateObj = new Date(objective.endDate);
   const now = new Date();
@@ -39,21 +41,21 @@ export const Card = ({ objective }: { objective: Objective }) => {
     : undefined;
 
   return (
-    <Pressable
-      style={({ pressed }) => [pressed && { backgroundColor: colors.gray[50] }]}
-    >
+    <Pressable className="active:bg-gray-50 dark:active:bg-dark-200">
       <Row
         align="center"
         justify="between"
-        className="px-4 py-3.5 border-t-[0.5px] border-gray-100"
+        className="px-4 py-3.5 border-t-[0.5px] border-gray-100 dark:border-dark-100"
       >
         <Row align="center" gap={3} className="w-8/12">
-          <Row className="bg-gray-100 rounded-lg p-1.5">
+          <Row className="bg-gray-100 dark:bg-dark-200 rounded-lg p-1.5">
             <SymbolView
               name="target"
               size={20}
               weight="bold"
-              tintColor={colors.gray.DEFAULT}
+              tintColor={
+                colorScheme === "light" ? colors.gray.DEFAULT : colors.gray[300]
+              }
             />
           </Row>
           <Col>
@@ -64,7 +66,11 @@ export const Card = ({ objective }: { objective: Objective }) => {
               <SymbolView
                 name="calendar"
                 size={19}
-                tintColor={colors.gray.DEFAULT}
+                tintColor={
+                  colorScheme === "light"
+                    ? colors.gray.DEFAULT
+                    : colors.gray[300]
+                }
               />
               <Text fontSize="sm" numberOfLines={1}>
                 {format(startDateObj, "MMM d")} → {format(endDateObj, "MMM d")}
