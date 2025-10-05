2c53983725a24e0a8b4b4709df198626c68141e8
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-05 18:13:20 +0300

    refactor: replace Pressable with ContextMenu in Header component for enhanced user interaction; implement responsive design with Tailwind and improve accessibility with color scheme support

--- apps/mobile/modules/notifications/components/header.tsx
+++ apps/mobile/modules/notifications/components/header.tsx
@@ -1,23 +1,57 @@
 import React from "react";
-import { Pressable } from "react-native";
 import { Row, Text } from "@/components/ui";
-import { SymbolView } from "expo-symbols";
 import { colors } from "@/constants";
+import { ContextMenu, Host, HStack, Button, Image } from "@expo/ui/swift-ui";
+import { cornerRadius, frame, glassEffect } from "@expo/ui/swift-ui/modifiers";
+import { useColorScheme } from "nativewind";
 
 export const Header = () => {
+  const { colorScheme } = useColorScheme();
   return (
     <Row justify="between" align="center" asContainer className="mb-3">
       <Text fontSize="2xl" fontWeight="semibold" color="black">
         Notifications
       </Text>
-      <Pressable
-        className="p-2 rounded-md"
-        style={({ pressed }) => [
-          pressed && { backgroundColor: colors.gray[50] },
-        ]}
-      >
-        <SymbolView name="ellipsis" tintColor={colors.dark[50]} />
-      </Pressable>
+
+      <Host matchContents>
+        <ContextMenu>
+          <ContextMenu.Items>
+            <Button systemImage="gear" onPress={() => {}}>
+              Notification settings
+            </Button>
+            <Button systemImage="checkmark.circle.fill" onPress={() => {}}>
+              Mark all as read
+            </Button>
+            <Button systemImage="delete.forward.fill" onPress={() => {}}>
+              Delete read
+            </Button>
+            <Button systemImage="trash.fill" onPress={() => {}}>
+              Delete all
+            </Button>
+          </ContextMenu.Items>
+          <ContextMenu.Trigger>
+            <HStack
+              modifiers={[
+                frame({ width: 40, height: 40 }),
+                glassEffect({
+                  glass: {
+                    variant: "regular",
+                  },
+                }),
+                cornerRadius(18),
+              ]}
+            >
+              <Image
+                systemName="ellipsis"
+                size={20}
+                color={
+                  colorScheme === "light" ? colors.dark[50] : colors.gray[300]
+                }
+              />
+            </HStack>
+          </ContextMenu.Trigger>
+        </ContextMenu>
+      </Host>
     </Row>
   );
 };
