d1c080fe106e41d50db603b4ca43e43a5ddd1429
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-06 17:33:09 +0300

    refactor: enhance OptionsSheet component with improved layout and context menu functionality for better user interaction

--- apps/mobile/modules/my-work/components/options-sheet.tsx
+++ apps/mobile/modules/my-work/components/options-sheet.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { BottomSheetModal } from "@/components/ui";
-import { Text, HStack } from "@expo/ui/swift-ui";
+import {
+  Text,
+  HStack,
+  Spacer,
+  ContextMenu,
+  Button,
+  Image,
+  VStack,
+} from "@expo/ui/swift-ui";
 import { colors } from "@/constants";
+import { opacity } from "@expo/ui/swift-ui/modifiers";
 
 export const OptionsSheet = ({
   isOpened,
@@ -11,13 +20,98 @@ export const OptionsSheet = ({
   setIsOpened: (isOpened: boolean) => void;
 }) => {
   return (
-    <BottomSheetModal isOpen={isOpened} onClose={() => setIsOpened(false)}>
-      <HStack>
-        <Text weight="semibold" color={colors.gray.DEFAULT} size={15}>
-          Appearance
-        </Text>
-      </HStack>
-      <Text>Hello, world!</Text>
+    <BottomSheetModal
+      isOpen={isOpened}
+      onClose={() => setIsOpened(false)}
+      spacing={28}
+    >
+      <VStack spacing={20}>
+        <HStack>
+          <Text>Grouping</Text>
+          <Spacer />
+          <HStack spacing={2}>
+            <ContextMenu>
+              <ContextMenu.Trigger>
+                <HStack spacing={3}>
+                  <Text color={colors.dark.DEFAULT}>Status</Text>
+                  <Image
+                    systemName="chevron.up.chevron.down"
+                    modifiers={[opacity(0.6)]}
+                    color={colors.dark.DEFAULT}
+                    size={14}
+                  />
+                </HStack>
+              </ContextMenu.Trigger>
+              <ContextMenu.Items>
+                <Button>Status</Button>
+                <Button>Priority</Button>
+                <Button>Assignee</Button>
+              </ContextMenu.Items>
+            </ContextMenu>
+          </HStack>
+        </HStack>
+        <HStack>
+          <Text>Ordering</Text>
+          <Spacer />
+          <HStack spacing={2}>
+            <ContextMenu>
+              <ContextMenu.Trigger>
+                <HStack spacing={3}>
+                  <Text color={colors.dark.DEFAULT}>Created</Text>
+                  <Image
+                    systemName="chevron.up.chevron.down"
+                    modifiers={[opacity(0.6)]}
+                    color={colors.dark.DEFAULT}
+                    size={14}
+                  />
+                </HStack>
+              </ContextMenu.Trigger>
+              <ContextMenu.Items>
+                <Button>Created</Button>
+                <Button>Updated</Button>
+                <Button>Deadline</Button>
+                <Button>Priority</Button>
+              </ContextMenu.Items>
+            </ContextMenu>
+          </HStack>
+        </HStack>
+        <HStack>
+          <Text>Order direction</Text>
+          <Spacer />
+          <HStack spacing={2}>
+            <ContextMenu>
+              <ContextMenu.Trigger>
+                <HStack spacing={3}>
+                  <Text color={colors.dark.DEFAULT}>Descending</Text>
+                  <Image
+                    systemName="chevron.up.chevron.down"
+                    modifiers={[opacity(0.6)]}
+                    color={colors.dark.DEFAULT}
+                    size={14}
+                  />
+                </HStack>
+              </ContextMenu.Trigger>
+              <ContextMenu.Items>
+                <Button>Descending</Button>
+                <Button>Ascending</Button>
+              </ContextMenu.Items>
+            </ContextMenu>
+          </HStack>
+        </HStack>
+      </VStack>
+      <VStack spacing={16} alignment="leading">
+        <Text modifiers={[opacity(0.75)]}>Display columns</Text>
+        <HStack spacing={12}>
+          <Button variant="bordered" color={colors.black}>
+            ID
+          </Button>
+          <Button variant="bordered" color={colors.black}>
+            Status
+          </Button>
+          <Button variant="plain">Assignee</Button>
+          <Button variant="plain">Priority</Button>
+        </HStack>
+      </VStack>
     </BottomSheetModal>
   );
 };
