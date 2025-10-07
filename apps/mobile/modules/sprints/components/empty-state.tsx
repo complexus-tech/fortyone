127b425fc4aeb9ee49d8e60219a8109fa83efdb8
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-07 19:40:16 +0300

    refactor: enhance EmptyState components with dark mode support and improved layout consistency

--- apps/mobile/modules/sprints/components/empty-state.tsx
+++ apps/mobile/modules/sprints/components/empty-state.tsx
@@ -3,6 +3,7 @@ import { Col, Row, Text } from "@/components/ui";
 import { SymbolView } from "expo-symbols";
 import { colors } from "@/constants";
 import { useTerminology } from "@/hooks/use-terminology";
+import { useColorScheme } from "nativewind";
 
 type EmptyStateProps = {
   title?: string;
@@ -10,19 +11,26 @@ type EmptyStateProps = {
 };
 
 export const EmptyState = ({ title, message }: EmptyStateProps) => {
+  const { colorScheme } = useColorScheme();
   const { getTermDisplay } = useTerminology();
 
   const defaultTitle = `No ${getTermDisplay("sprintTerm", { variant: "plural" })} found`;
   const defaultMessage = `There are no ${getTermDisplay("sprintTerm", { variant: "plural" })} to display at the moment.`;
 
   return (
-    <Col justify="center" align="center" className="flex-1" asContainer>
+    <Col justify="center" align="center" className="flex-1 pt-56" asContainer>
       <Row
         align="center"
         justify="center"
-        className="size-18 rounded-full bg-gray-50 mb-6"
+        className="size-18 rounded-full bg-gray-50 mb-6 dark:bg-dark-300"
       >
-        <SymbolView name="memories" size={36} tintColor={colors.gray.DEFAULT} />
+        <SymbolView
+          name="memories"
+          size={36}
+          tintColor={
+            colorScheme === "light" ? colors.gray.DEFAULT : colors.gray[200]
+          }
+        />
       </Row>
       <Text fontSize="xl" fontWeight="semibold" className="mb-4 text-center">
         {title || defaultTitle}
