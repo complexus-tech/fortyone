2eaec74a4a7a1b4eb8d822580bfa6a9479896253
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-08 11:49:49 +0300

    refactor: integrate Tabs into ActivitiesSkeleton for improved organization and user navigation during data fetching

--- apps/mobile/modules/story/components/activities-skeleton.tsx
+++ apps/mobile/modules/story/components/activities-skeleton.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { Col } from "@/components/ui";
+import { Col, Tabs } from "@/components/ui";
 import { ActivitySkeleton } from "./activity-skeleton";
 
 export const ActivitiesSkeleton = () => {
   return (
-    <Col asContainer>
-      {Array.from({ length: 7 }).map((_, index) => (
-        <ActivitySkeleton key={index} />
-      ))}
-    </Col>
+    <Tabs defaultValue="updates">
+      <Tabs.List className="mb-2">
+        <Tabs.Tab value="updates" className="py-2 px-4">
+          Updates
+        </Tabs.Tab>
+        <Tabs.Tab value="comments" className="py-2 px-4">
+          Comments
+        </Tabs.Tab>
+      </Tabs.List>
+      <Tabs.Panel value="updates">
+        <Col asContainer>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <ActivitySkeleton key={index} />
+          ))}
+        </Col>
+      </Tabs.Panel>
+    </Tabs>
   );
 };
