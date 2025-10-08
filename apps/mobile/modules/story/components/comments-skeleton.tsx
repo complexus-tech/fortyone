ae943f5dc26821fbd629dfb266a2f2168d1f0f80
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-08 11:45:13 +0300

    refactor: increase the number of loading skeletons in CommentsSkeleton for enhanced user experience during data fetching

--- apps/mobile/modules/story/components/comments-skeleton.tsx
+++ apps/mobile/modules/story/components/comments-skeleton.tsx
@@ -5,7 +5,7 @@ import { CommentSkeleton } from "./comment-skeleton";
 export const CommentsSkeleton = () => {
   return (
     <Col asContainer>
-      {Array.from({ length: 3 }).map((_, index) => (
+      {Array.from({ length: 6 }).map((_, index) => (
         <CommentSkeleton key={index} />
       ))}
     </Col>
