dbb82cf5d2c89fbc4ae4e1b0a43a5d91044a43a0
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-13 00:37:07 +0300

    refactor: consolidate authentication logic into login and signup pages, removing the Layout component for improved clarity and maintainability

--- apps/landing/src/app/(onboarding)/(auth)/login/page.tsx
+++ apps/landing/src/app/(onboarding)/(auth)/login/page.tsx
@@ -1,10 +1,24 @@
 import type { Metadata } from "next";
 import { AuthLayout } from "@/modules/auth";
+import { auth } from "@/auth";
+import { getProfile } from "@/lib/queries/profile";
+import { getWorkspaces } from "@/lib/queries/get-workspaces";
+import { redirect } from "next/navigation";
+import { getRedirectUrl } from "@/utils";
 
 export const metadata: Metadata = {
   title: "Login - FortyOne",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  if (session) {
+    const [workspaces, profile] = await Promise.all([
+      getWorkspaces(session?.token || ""),
+      getProfile(session),
+    ]);
+    redirect(getRedirectUrl(workspaces, [], profile?.lastUsedWorkspaceId));
+  }
   return <AuthLayout page="login" />;
 }
