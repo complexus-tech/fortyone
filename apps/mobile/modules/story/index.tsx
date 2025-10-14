22b8a38667fb8046f9b2ef40a739fb68757586e1
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-14 23:23:53 +0300

    feat: add BottomSheetModal with DateTimePicker to Story component for date selection functionality

--- apps/mobile/modules/story/index.tsx
+++ apps/mobile/modules/story/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView } from "react-native";
 import { Properties } from "./components/properties";
 import { useGlobalSearchParams } from "expo-router";
@@ -7,10 +7,13 @@ import { Activity } from "./components/activity";
 import { Description } from "./components/descrition";
 import { Title } from "./components/title";
 import { StorySkeleton } from "./components/story-skeleton";
+import { BottomSheetModal } from "@/components/ui";
+import { Text, DateTimePicker } from "@expo/ui/swift-ui";
 
 export const Story = () => {
   const { storyId } = useGlobalSearchParams<{ storyId: string }>();
   const { data: story, isPending } = useStory(storyId);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   if (isPending) {
     return <StorySkeleton />;
@@ -25,6 +28,17 @@ export const Story = () => {
       <Properties story={story!} />
       <Description story={story!} />
       <Activity story={story!} />
+      <BottomSheetModal isOpen spacing={10} onClose={() => {}}>
+        <DateTimePicker
+          onDateSelected={(date) => {
+            setSelectedDate(date);
+          }}
+          displayedComponents="date"
+          initialDate={selectedDate.toISOString()}
+          variant="graphical"
+          color="red"
+        />
+      </BottomSheetModal>
     </ScrollView>
   );
 };
