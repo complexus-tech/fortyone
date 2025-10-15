eb4b95a922f5b51404567d0377e26c245ccbb0ca
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-15 18:59:09 +0300

    refactor: enhance description component to support dynamic text color based on color scheme for improved readability

--- apps/mobile/modules/story/components/description.tsx
+++ apps/mobile/modules/story/components/description.tsx
@@ -3,9 +3,12 @@ import { Row, Text } from "@/components/ui";
 import { DetailedStory } from "@/modules/stories/types";
 import RenderHtml from "react-native-render-html";
 import { useWindowDimensions } from "react-native";
+import { useColorScheme } from "nativewind";
+import { colors } from "@/constants";
 
 export const Description = ({ story }: { story: DetailedStory }) => {
   const { width } = useWindowDimensions();
+  const { colorScheme } = useColorScheme();
   if (!story.description) {
     return (
       <Row asContainer className="mb-6 mt-2">
@@ -27,6 +30,7 @@ export const Description = ({ story }: { story: DetailedStory }) => {
           fontWeight: 500,
           lineHeight: 23,
           opacity: 0.8,
+          color: colorScheme === "dark" ? colors.white : colors.black,
         }}
         contentWidth={width}
         source={source}
