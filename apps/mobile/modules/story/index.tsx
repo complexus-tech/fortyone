4f7deee4ed3e176e0d4fcdb53da5ac71ade0c471
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-15 19:06:23 +0300

    refactor: remove unused useState import in story module for cleaner code

--- apps/mobile/modules/story/index.tsx
+++ apps/mobile/modules/story/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ScrollView } from "react-native";
 import { Properties } from "./components/properties";
 import { useGlobalSearchParams } from "expo-router";
