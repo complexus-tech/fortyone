24dcc843b6071ca9b6a5656928002554ad454be0
Joseph Mukorivo <josemukorivo@gmail.com>
2025-10-16 22:41:02 +0300

    feat: enhance NewStory component with ScrollView and SafeContainer for improved layout and theme support

--- apps/mobile/modules/new-story/index.tsx
+++ apps/mobile/modules/new-story/index.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from "react-native";
+import { Text, ScrollView } from "react-native";
+import { SafeContainer } from "@/components/ui";
+import { colors } from "@/constants";
+import { useTheme } from "@/hooks";
 
 export const NewStory = () => {
+  const { resolvedTheme } = useTheme();
   return (
-    <View>
-      <Text>New Story</Text>
-    </View>
+    <SafeContainer isFull>
+      <ScrollView
+        style={{
+          paddingTop: 44,
+          flex: 1,
+          backgroundColor:
+            resolvedTheme === "light" ? colors.white : colors.dark[200],
+        }}
+        contentContainerStyle={{ paddingBottom: 120 }}
+      >
+        <Text>New Story</Text>
+      </ScrollView>
+    </SafeContainer>
   );
 };
