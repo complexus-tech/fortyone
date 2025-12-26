218f1c4ea4ceceb21ea047e669588d704538db2d
Joseph Mukorivo <josemukorivo@gmail.com>
2025-12-26 18:53:27 +0200

    chore: remove ui3 package configuration files and components to streamline the package

--- packages/ui3/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import { config } from "eslint-config-custom/react-internal";
-
-/** @type {import("eslint").Linter.Config} */
-export default config;
--- packages/ui3/src/Debug/Debug.tsx
+++
@@ -1,6 +0,0 @@
-export const Debug = ({ data }: { data: any }) => {
-  if (process.env.NODE_ENV === "production") {
-    return null;
-  }
-  return <pre className="dark:text-white">{JSON.stringify(data, null, 2)}</pre>;
-};
