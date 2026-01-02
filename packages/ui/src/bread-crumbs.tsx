2c08d619bc2c63b4dd61516214422f2410114f2a
Joseph Mukorivo <josemukorivo@gmail.com>
2026-01-02 14:57:01 +0200

    style: adjust sidebar color and replace breadcrumb separator icon with a slash.

--- packages/ui/src/bread-crumbs.tsx
+++ packages/ui/src/bread-crumbs.tsx
@@ -26,8 +26,7 @@ export const BreadCrumbs = ({ breadCrumbs, className }: BreadCrumbsProps) => {
           className={cn(
             "flex items-center gap-1.5 font-medium group first-letter:uppercase transition",
             {
-              "text-text-muted":
-                idx + 1 === breadCrumbs.length,
+              "text-text-muted": idx + 1 === breadCrumbs.length,
               "pointer-events-none": !url,
             },
             className
@@ -37,13 +36,20 @@ export const BreadCrumbs = ({ breadCrumbs, className }: BreadCrumbsProps) => {
             {icon && <span className="group-hover:text-primary">{icon}</span>}
             <span className="group-hover:text-primary line-clamp-1">
               {name}
+            </span>{" "}
+            <span
+              className={cn("opacity-80", {
+                hidden: idx + 1 === breadCrumbs.length,
+              })}
+            >
+              /
             </span>
-            <ArrowRight2Icon
+            {/* <ArrowRight2Icon
               strokeWidth={3}
               className={cn("h-4 opacity-80", {
                 hidden: idx + 1 === breadCrumbs.length,
               })}
-            />
+            /> */}
           </>
         </Link>
       ))}
