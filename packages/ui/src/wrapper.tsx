1a60a98b101eaf8bc3600dc81bf69a80101844f3
Joseph Mukorivo <josemukorivo@gmail.com>
2026-01-02 16:37:08 +0200

    style: Adjust shadow color value in shared styles and update button cursor styling for improved visual consistency

--- packages/ui/src/wrapper.tsx
+++ packages/ui/src/wrapper.tsx
@@ -5,7 +5,7 @@ export const Wrapper = ({ children, className, ...rest }: BoxProps) => {
   return (
     <Box
       className={cn(
-        "rounded-2xl border border-border py-4 px-4 md:px-5 shadow-lg shadow-shadow bg-surface",
+        "rounded-2xl border-[0.5px] border-border py-4 px-4 md:px-5 shadow-lg shadow-shadow bg-surface",
         className
       )}
       {...rest}
