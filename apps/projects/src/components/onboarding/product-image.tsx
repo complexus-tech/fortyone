d316cfe6208edbd774cabc611182c2ffd36a526b
Joseph Mukorivo <josemukorivo@gmail.com>
2026-01-18 13:54:35 +0200

    refactor: Update product image component with improved styling and responsive design adjustments

--- apps/projects/src/components/onboarding/product-image.tsx
+++ apps/projects/src/components/onboarding/product-image.tsx
@@ -18,45 +18,45 @@ export const ProductImage = () => {
         src="/images/mesh.webp"
       />
       <Container className="absolute inset-0 z-10 flex flex-col justify-between pt-12 pb-16 md:px-16 dark:text-black">
-        <Logo className="h-7 dark:text-black" />
+        <Link href="/"><Logo className="h-9 dark:text-black" /></Link>
         <Box>
           <Text
             as="h2"
-            className="mb-4 font-mono font-semibold tracking-wider uppercase dark:text-black"
+            className="mb-4 font-mono font-semibold antialiased tracking-wider uppercase dark:text-black text-lg"
           >
             [Built for builders]
           </Text>
           <Text
             as="h3"
-            className="mb-10 text-5xl leading-[1.2] font-semibold dark:text-black"
+            className="mb-10 text-6xl leading-[1.2] font-semibold dark:text-black"
           >
             Plan, track, deliver with the project management tool your team will
             love.
           </Text>
           <Box className="3xl:gap-16 flex gap-8">
             <Link href="https://x.com/fortyoneapp" target="_blank">
-              <TwitterIcon className="text-foreground dark:text-black dark:hover:text-white" />
+              <TwitterIcon className="text-foreground h-7 dark:text-black dark:hover:text-white" />
             </Link>
             <Link
               href="https://www.linkedin.com/company/complexus-tech/"
               target="_blank"
             >
-              <LinkedinIcon className="text-foreground dark:text-black dark:hover:text-white" />
+              <LinkedinIcon className="text-foreground h-7 dark:text-black dark:hover:text-white" />
             </Link>
             <Link
               href="https://www.instagram.com/complexus_tech/"
               target="_blank"
             >
-              <InstagramIcon className="text-foreground dark:text-black dark:hover:text-white" />
+              <InstagramIcon className="text-foreground h-7 dark:text-black dark:hover:text-white" />
             </Link>
             <Link
               href="https://www.facebook.com/complexus.tech"
               target="_blank"
             >
-              <FacebookIcon className="text-foreground dark:text-black dark:hover:text-white" />
+              <FacebookIcon className="text-foreground h-7 dark:text-black dark:hover:text-white" />
             </Link>
           </Box>
-          <Text className="mt-10 text-[0.95rem] dark:text-black">
+          <Text className="mt-10 text-lg dark:text-black">
             By{" "}
             {pathname?.includes("signup")
               ? "signing up"
